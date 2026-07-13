1a8e51734af0d7cb614ee915f12c69b1bcf848d6
Nathan <101719115+n4thyan@users.noreply.github.com>
2026-07-13 13:59:42 +0100

    Add UWP assembly metadata

---
+++ uwp/BinWeevilsRewrittenXbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bin Weevils Rewritten Xbox")]
+[assembly: AssemblyDescription("Experimental Xbox UWP client for Bin Weevils Rewritten")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("n4thyan")]
+[assembly: AssemblyProduct("Bin Weevils Rewritten Xbox")]
+[assembly: AssemblyCopyright("CC0 client fork; upstream game assets and services remain property of their respective owners")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
